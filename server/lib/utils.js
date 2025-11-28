a45a8938f352a73dd9dbbdebe73c131671b52e5f
Aditya <24f2005506@ds.study.iitm.ac.in>
2025-11-28 23:32:16 +0530

    Added Admin

--- server/lib/utils.js
+++ server/lib/utils.js
@@ -2,7 +2,10 @@ import jwt from "jsonwebtoken";
 
 export const generateToken = (user) => {
   return jwt.sign(
-    { userId: user._id },
+    {
+      userId: user._id,     // ✅ MUST be userId to match middleware
+      isAdmin: user.isAdmin
+    },
     process.env.JWT_SECRET,
     { expiresIn: "7d" }
   );
