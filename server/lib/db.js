84c72b9d89227acc89dc200215a9651745b3d0ad
Aditya <24f2005506@ds.study.iitm.ac.in>
2025-11-29 00:52:41 +0530

    Added f admin

--- server/lib/db.js
+++ server/lib/db.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    try {
-        mongoose.connection.on('connected', () => console.log('Database Connected'));
-        await mongoose.connect(`${process.env.MONGODB_URI}`);
-    } catch (error) {
-        console.log(error);
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+
+    console.log("Database Connected to:", mongoose.connection.name);
+  } catch (error) {
+    console.log("DB ERROR:", error.message);
   }
-}
+};
