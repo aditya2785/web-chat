886a4231f552d1aa5d50b104a73d9b6ba6e6e592
Aditya <24f2005506@ds.study.iitm.ac.in>
2025-11-29 00:20:41 +0530

    Added Admin

--- server/hash.js
+++ server/hash.js
@@ -1,7 +1,7 @@
 import bcrypt from "bcryptjs";
 
 const run = async () => {
-  const password = "aditya"; // <--- your new password
+  const password = "NewAdmin123"; // <--- your new password
   const hash = await bcrypt.hash(password, 10);
   console.log(hash);
 };
