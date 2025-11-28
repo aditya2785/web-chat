e02d5fd6ff6c2a74100f5c7c3660fbc1bed1497a
Aditya <24f2005506@ds.study.iitm.ac.in>
2025-11-28 22:52:53 +0530

    Fix des fully

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -31,8 +31,8 @@ const LoginPage = () => {
 
   return (
     <div>
-      <div className='min-h-screen bg-cover bg-center flex items-center
-      justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl'>
+      <div className="min-h-screen w-full flex items-center justify-center bg-[#0f172a]">
+
         <img src={assets.logo_big} alt='' className='w-[min(30vw,250px)]' />
 
         <form 
